c737e590af5027da4bbe1817e9d551111b6f704c
congnghia0609 <congnghia0609@gmail.com>
2020-10-06 17:24:07 +0700

    add config

--- main.go
+++ main.go
@@ -7,13 +7,75 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"ntc-gfastserver/server"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"runtime"
+	"syscall"
+
+	"github.com/congnghia0609/ntc-gconf/nconf"
+	"github.com/natefinch/lumberjack"
 )
 
+// initNConf init file config
+func initNConf() {
+	_, b, _, _ := runtime.Caller(0)
+	wdir := filepath.Dir(b)
+	fmt.Println("wdir:", wdir)
+	nconf.Init(wdir)
+}
+
+// https://github.com/natefinch/lumberjack
+func initLogger() {
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   "/data/log/ntc-gfastserver/ntc-gfastserver.log",
+		MaxSize:    10,   // 10 megabytes. Defaults to 100 MB.
+		MaxBackups: 3,    // maximum number of old log files to retain.
+		MaxAge:     28,   // maximum number of days to retain old log files
+		Compress:   true, // disabled by default
+	})
+}
+
+// increaseLimit increase resources limitations: ulimit -a
+func increaseLimit() {
+	var rlimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
+		panic(err)
+	}
+	rlimit.Cur = rlimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rlimit); err != nil {
+		panic(err)
+	}
+	log.Printf("rlimit.Max = %d\n", rlimit.Max)
+	log.Printf("rlimit.Cur = %d\n", rlimit.Cur)
+}
+
 func main() {
+	////// -------------------- Init System -------------------- //////
+	// Increase resources limitations
+	increaseLimit()
+
+	// Init NConf
+	initNConf()
+
+	//// Init Logger
+	if "development" != nconf.GetEnv() {
+		log.Printf("============== LogFile: /data/log/ntc-gfastserver/ntc-gfastserver.log")
+		initLogger()
+	}
+
+	// Enable pprof hooks
+	go func() {
+		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+			log.Fatalf("pprof failed: %v", err)
+		}
+	}()
+
 	////// -------------------- Start WebServer -------------------- //////
 	// StartWebServer
 	go server.StartWebServer("webserver")
--- server/webserver.go
+++ server/webserver.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/congnghia0609/ntc-gconf/nconf"
 	"github.com/fasthttp/router"
 	"github.com/valyala/fasthttp"
 )
@@ -21,11 +22,28 @@ func Index(ctx *fasthttp.RequestCtx) {
 
 // Hello handler
 func Hello(ctx *fasthttp.RequestCtx) {
+	// set some headers and status code first
+	ctx.SetContentType("text/plain; charset=utf-8")
+	ctx.SetStatusCode(fasthttp.StatusOK)
+
+	// then write the first part of body
 	fmt.Fprintf(ctx, "Hello, %s!\n", ctx.UserValue("name"))
+
+	// then set more headers
+	ctx.Response.Header.Set("Foo-Bar", "baz")
+
+	// then write more body
+	fmt.Fprintf(ctx, "this is the second part of body\n")
+
+	// // then override already written body
+	// ctx.SetBody([]byte("this is completely new body contents"))
 }
 
 // StartWebServer start WebServer
 func StartWebServer(name string) {
+	// Config
+	c := nconf.GetConfig()
+	address := c.GetString(name + ".addr")
 
 	// Setup Router Handlers.
 	r := router.New()
@@ -35,9 +53,9 @@ func StartWebServer(name string) {
 	// Serve static files from the ./public directory
 	r.NotFound = fasthttp.FSHandler("./public", 0) // http://localhost:8080/css/main.css
 
-	log.Printf("======= WebServer[%s] is running on host: %s", name, ":8080")
+	log.Printf("======= WebServer[%s] is running on host: %s", name, address)
 	// log.Fatal(fasthttp.ListenAndServe(":8080", r.Handler))
-	err := fasthttp.ListenAndServe(":8080", r.Handler)
+	err := fasthttp.ListenAndServe(address, r.Handler)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}

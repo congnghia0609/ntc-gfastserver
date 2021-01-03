72473fe63a85ce4d1dc5c5ee510e581c35ca540e
congnghia0609 <congnghia0609@gmail.com>
2021-01-03 22:33:59 +0700

    add mongo client

--- main.go
+++ main.go
@@ -9,11 +9,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	_ "net/http/pprof"
-	"ntc-gfastserver/server"
-	"os"
-	"os/signal"
 	"path/filepath"
 	"runtime"
 	"syscall"
@@ -55,11 +51,42 @@ func increaseLimit() {
 	log.Printf("rlimit.Cur = %d\n", rlimit.Cur)
 }
 
+// func main() {
+// 	////// -------------------- Init System -------------------- //////
+// 	// Increase resources limitations
+// 	increaseLimit()
+
+// 	// Init NConf
+// 	initNConf()
+
+// 	//// Init Logger
+// 	if "development" != nconf.GetEnv() {
+// 		log.Printf("============== LogFile: /data/log/ntc-gfastserver/ntc-gfastserver.log")
+// 		initLogger()
+// 	}
+
+// 	// Enable pprof hooks
+// 	go func() {
+// 		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+// 			log.Fatalf("pprof failed: %v", err)
+// 		}
+// 	}()
+
+// 	////// -------------------- Start WebServer -------------------- //////
+// 	// StartWebServer
+// 	go server.StartWebServer("webserver")
+
+// 	// Hang thread Main.
+// 	c := make(chan os.Signal, 1)
+// 	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C) SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+// 	signal.Notify(c, os.Interrupt)
+// 	// Block until we receive our signal.
+// 	<-c
+// 	log.Println("################# End Main #################")
+// }
+
 func main() {
 	////// -------------------- Init System -------------------- //////
-	// Increase resources limitations
-	increaseLimit()
-
 	// Init NConf
 	initNConf()
 
@@ -69,22 +96,7 @@ func main() {
 		initLogger()
 	}
 
-	// Enable pprof hooks
-	go func() {
-		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
-			log.Fatalf("pprof failed: %v", err)
-		}
-	}()
+	// Code test here.
 
-	////// -------------------- Start WebServer -------------------- //////
-	// StartWebServer
-	go server.StartWebServer("webserver")
-
-	// Hang thread Main.
-	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C) SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
-	// Block until we receive our signal.
-	<-c
 	log.Println("################# End Main #################")
 }
---
+++ mdb/mdb.go
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author nghiatc
+ * @since Jan 3, 2021
+ */
+
+package mdb
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// GetClient return mongo client
+// https://pkg.go.dev/go.mongodb.org/mongo-driver/mongo#Connect
+func GetClient() *mongo.Client {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	return client
+}
+
+// Close disconnect mongo client
+func Close(client *mongo.Client) {
+	if err := client.Disconnect(context.Background()); err != nil {
+		log.Println(err)
+	}
+}
